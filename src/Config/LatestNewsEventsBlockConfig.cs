c5638060044c61c5de89ca203e8c59abc5822833
Dmitry Titarenko <dmitry.titarenko@graph.uk>
2019-02-20 18:41:35 +0500

    ST-23: use constants in config

--- src/Config/LatestNewsEventsBlockConfig.cs
+++ src/Config/LatestNewsEventsBlockConfig.cs
@@ -2,26 +2,26 @@ namespace Graph.Components.LatestNewsEventsBlock
 {
 	public static class LatestNewsEventsBlockConfig
 	{
-		public static string HomePageId => "1067";
+		public static string HomePageId => "ID_HERE";
 
 		public static class NewsConfig
 		{
 			public const string PageAlias = "NewsPage";
-			public static string Title => "title";
-			public static string Description => "summary";
-			public static string Date => "date";
-			public static string Image => "image";
-			public static string Eyebrow => "eyebrow";
+			public const string Title = "title";
+			public const string Description = "summary";
+			public const string Date = "date";
+			public const string Image = "image";
+			public const string Eyebrow = "eyebrow";
 		}
 
 		public static class EventsConfig
 		{
 			public const string PageAlias = "EventPage";
-			public static string Title => "title";
-			public static string StartDate => "startDate";
-			public static string EndDate => "endDate";
-			public static string Eyebrow => "eyebrow";
-			public static string Location => "location";
+			public const string Title = "title";
+			public const string StartDate = "startDate";
+			public const string EndDate = "endDate";
+			public const string Eyebrow = "eyebrow";
+			public const string Location = "location";
 		}
 	}
 }
